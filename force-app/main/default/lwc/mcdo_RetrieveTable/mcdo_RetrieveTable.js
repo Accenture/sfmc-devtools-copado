85873b10e16132b208a5040aea1a3e01b4a6f7fe
Jörn Berkefeld <JoernBerkefeld@users.noreply.github.com>
2022-10-13 15:26:25 +0200

    #148 pr review fix
    remove no longer needed method

--- force-app/main/default/lwc/mcdo_RetrieveTable/mcdo_RetrieveTable.js
+++ force-app/main/default/lwc/mcdo_RetrieveTable/mcdo_RetrieveTable.js
@@ -473,12 +473,4 @@ export default class mcdo_RetrieveTable extends LightningElement {
         this.showTable = !isLoading;
         this.refreshButtonDisabled = isLoading;
     }
-    //  Validate JSON string
-    isJSON(tempData) {
-        var str = tempData;
-        str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, "@");
-        str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]");
-        str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, "");
-        return /^[\],:{}\s]*$/.test(str);
-    }
 }
