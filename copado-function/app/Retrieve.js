e838b943bfc6b760ef81bb39b4ca5dcd9e869efe
Jörn Berkefeld <joern.berkefeld@accenture.com>
2022-10-25 17:11:21 +0200

    #246: link to global install by default

--- copado-function/app/Retrieve.js
+++ copado-function/app/Retrieve.js
@@ -41,12 +41,11 @@ const CONFIG = {
     // generic
     configFilePath: '.mcdevrc.json',
     debug: process.env.debug === 'true' ? true : false,
-    localDev: process.env.LOCAL_DEV === 'true' ? true : false,
     installMcdevLocally: process.env.installMcdevLocally === 'true' ? true : false,
     envId: process.env.envId,
     mainBranch: process.env.main_branch,
     mcdev_exec: 'node ./node_modules/mcdev/lib/cli.js', // !works only after changing the working directory!
-    mcdevVersion: process.env.mcdev_version,
+    mcdevVersion: process.env.mcdev_version || '/usr/local/lib/node_modules/mcdev',
     metadataFilePath: 'mcmetadata.json', // do not change - LWC depends on it!
     source_mid: process.env.source_mid,
     tmpDirectory: '../tmp',
@@ -127,13 +126,11 @@ async function run() {
     }
 
     try {
-        if (CONFIG.installMcdevLocally) {
-            Log.info('');
-            Log.info('Preparing');
-            Log.info('===================');
-            Log.info('');
-            Util.provideMCDevTools();
-        }
+        Log.info('');
+        Log.info('Preparing');
+        Log.info('===================');
+        Log.info('');
+        Util.provideMCDevTools();
 
         Log.info('');
         Log.info('Initialize project');
@@ -374,7 +371,7 @@ class Util {
             Util.execCommand('Initializing npm', ['npm init -y'], 'Completed initializing NPM');
         }
         let installer;
-        if (CONFIG.localDev) {
+        if (!CONFIG.installMcdevLocally) {
             installer = CONFIG.mcdevVersion;
         } else if (CONFIG.mcdevVersion.charAt(0) === '#') {
             // assume branch of mcdev's git repo shall be loaded
@@ -647,18 +644,9 @@ class Retrieve {
      */
     static async retrieveChangelog(sourceBU) {
         // * dont use CONFIG.tempDir here to allow proper resolution of required package in VSCode
-        let mcdev;
-        let Definition;
-        let MetadataType;
-        if (!CONFIG.installMcdevLocally) {
-            mcdev = require('/usr/local/lib/node_modules/mcdev/lib/');
-            Definition = require('/usr/local/lib/node_modules/mcdev/lib/MetadataTypeDefinitions');
-            MetadataType = require('/usr/local/lib/node_modules/mcdev/lib/MetadataTypeInfo');
-        } else {
-            mcdev = require('../tmp/node_modules/mcdev/lib/');
-            Definition = require('../tmp/node_modules/mcdev/lib/MetadataTypeDefinitions');
-            MetadataType = require('../tmp/node_modules/mcdev/lib/MetadataTypeInfo');
-        }
+        const mcdev = require('../tmp/node_modules/mcdev/lib/');
+        const Definition = require('../tmp/node_modules/mcdev/lib/MetadataTypeDefinitions');
+        const MetadataType = require('../tmp/node_modules/mcdev/lib/MetadataTypeInfo');
         if (!CONFIG.debug) {
             // disable any non-errors originating in mcdev from being printed into the main copado logfile
             mcdev.setLoggingLevel({ silent: true });
