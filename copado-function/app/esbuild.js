be38859656ee50134d1bcc3e2c568ef52acbe166
Jörn Berkefeld <joern.berkefeld@accenture.com>
2022-11-16 11:37:38 +0100

    #291: added detailed banner to dist-files

--- copado-function/app/esbuild.js
+++ copado-function/app/esbuild.js
@@ -1,5 +1,7 @@
 /* eslint-disable unicorn/prefer-top-level-await */
 const esbuild = require('esbuild');
+const packageJson = require('../../package.json');
+const eslintrc = require('./.eslintrc.json');
 const path = require('node:path');
 const { esbuildPluginVersionInjector } = require('esbuild-plugin-version-injector');
 
@@ -7,27 +9,49 @@ const { esbuildPluginVersionInjector } = require('esbuild-plugin-version-injecto
     // console.log(process.cwd());
     // console.log(path.resolve(__dirname));
     let result;
-    try {
-        result = await esbuild.build({
-            entryPoints: ['Retrieve.fn.js', 'Commit.fn.js', 'Deploy.fn.js', 'McdevInit.fn.js'],
-            bundle: true,
-            platform: 'node',
-            external: ['../tmp/*'],
-            preserveSymlinks: true,
-            outdir: '../dist',
-            absWorkingDir: path.resolve(__dirname),
-            metafile: true,
-            plugins: [esbuildPluginVersionInjector()],
-            target: ['es2020', 'node14.16'],
-        });
-    } catch (ex) {
-        console.error('Build failed:', ex.message); // eslint-disable-line no-console
-        process.exitCode = 1;
-    }
-    try {
-        const analyzeText = await esbuild.analyzeMetafile(result.metafile);
-        console.log(analyzeText); // eslint-disable-line no-console
-    } catch (ex) {
-        console.warn('Analyze failed:', ex.message); // eslint-disable-line no-console
+    const ecmaVersion = 'es' + eslintrc.parserOptions.ecmaVersion;
+    const peerDependencies = Object.keys(packageJson.peerDependencies).map(
+        (name) => name + '@' + packageJson.peerDependencies[name]
+    );
+    const copadoDependencies = Object.keys(packageJson.copadoDependencies).map(
+        (name) => name + '@' + packageJson.copadoDependencies[name]
+    );
+    peerDependencies.push(...copadoDependencies);
+    const absWorkingDir = path.resolve(__dirname);
+    for (const entryPoint of ['Retrieve.fn.js', 'Commit.fn.js', 'Deploy.fn.js', 'Init.fn.js']) {
+        try {
+            result = await esbuild.build({
+                entryPoints: [entryPoint],
+                bundle: true,
+                banner: {
+                    js:
+                        `\n/*\n * ${packageJson.name} v${packageJson.version}\n` +
+                        ` * Function: ${entryPoint}\n` +
+                        ` * Dependenies: ${peerDependencies.join(', ')}\n` +
+                        ` * Homepage: ${packageJson.homepage}\n` +
+                        ` * Support: ${packageJson.bugs.url}\n` +
+                        ` * Git-Repository: ${packageJson.repository.url}\n` +
+                        ` * Copyright (c) 2022 Accenture. ${packageJson.license} licensed\n` +
+                        '*/\n\n',
+                },
+                platform: 'node',
+                external: ['../tmp/*'],
+                preserveSymlinks: true,
+                outdir: '../dist',
+                absWorkingDir: absWorkingDir,
+                metafile: true,
+                plugins: [esbuildPluginVersionInjector()],
+                target: [ecmaVersion, 'node14.16'],
+            });
+        } catch (ex) {
+            console.error('Build failed:', ex.message); // eslint-disable-line no-console
+            process.exitCode = 1;
+        }
+        try {
+            const analyzeText = await esbuild.analyzeMetafile(result.metafile);
+            console.log(analyzeText); // eslint-disable-line no-console
+        } catch (ex) {
+            console.warn('Analyze failed:', ex.message); // eslint-disable-line no-console
+        }
     }
 })();
