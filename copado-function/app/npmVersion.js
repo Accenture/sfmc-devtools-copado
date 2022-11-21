d229fd69f85f006d7d8be601cab5572275aa3794
Jörn Berkefeld <joern.berkefeld@accenture.com>
2022-11-21 13:47:37 +0100

    #0: improve version release script

---
+++ copado-function/app/npmVersion.js
@@ -0,0 +1,45 @@
+/* eslint-disable unicorn/prefer-top-level-await */
+const execSync = require('node:child_process').execSync;
+
+const args = process.argv.slice(2);
+const versionIncreaseType = args[0];
+
+/**
+ *
+ * @param command
+ */
+function exec(command) {
+    console.log('⚡ ' + command);
+    execSync(command, {
+        stdio: [0, 1, 2],
+        stderr: 'inherit',
+    });
+}
+(async () => {
+    if (!['major', 'minor', 'patch'].includes(versionIncreaseType)) {
+        throw new Error(
+            'Invalid version increase type. Please use one of: major, minor, patch (found: ' +
+                versionIncreaseType +
+                ')'
+        );
+    }
+    // increase version in package.json and package-lock.json without creating a git commit or adding a git tag
+    exec('npm version --no-git-tag-version ' + versionIncreaseType);
+
+    // re-build the project to ensure the new versions are mentioned in the build
+    exec('npm run build:docs');
+
+    // stage changes
+    exec('git add copado-function/dist/*');
+    exec('git add package.json');
+    exec('git add package-lock.json');
+
+    // get the new version number
+    const packageJson = require('../../package.json');
+    const newVersion = packageJson.version;
+
+    // commit changes without commit-hooks
+    exec(`git commit -n -m "${newVersion}"`);
+
+    exec('git tag -a v' + newVersion + ' -m "Release v' + newVersion + '"');
+})();
